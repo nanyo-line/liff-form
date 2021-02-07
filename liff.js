367e6fe8479063c4d3c6aca3bc453abcdc491ebb
nanyo-line <78678974+nanyo-line@users.noreply.github.com>
2021-02-07 21:13:40 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -27,11 +27,8 @@ function sendText(text) {
     liff.sendMessages([{
         'type': 'text',
         'text': text
-    },{
-        "type": "sticker",
-        "packageId": "11539",
-        "stickerId": "52114113"
-    }]).then(function () {
+    },
+    ]).then(function () {
         liff.closeWindow();
     }).catch(function (error) {
         window.alert('Failed to send message ' + error);
