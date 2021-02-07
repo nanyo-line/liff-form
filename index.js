5d1a19b9d61fbf6ce7eb4075f756383327e81f9e
nanyo-line <78678974+nanyo-line@users.noreply.github.com>
2021-02-07 20:41:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(function () {
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
         
-        //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
+        //var msg = `${key}\n朝礼/不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
         var msg = `${key}\n理由：${reasons}`;
         sendText(msg);
 
